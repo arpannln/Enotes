b5d8fe404cf2183960c6bc307c9cdd228509994c
Arpan NLN <arpannln@ucla.edu>
2018-07-16 12:21:44 -0700

    yay i now have a plugin to add red boxes to pages

--- content.js
+++ content.js
@@ -11,6 +11,21 @@
 //     }
 //   }
 // );
+
+let body = document.querySelector('body');
+console.log(body);
+var child = document.createElement('div');
+let modalStyles = `position: fixed;
+                  display: flex;
+                  width: 100px;
+                  height: 100px;
+                  z-index: 1000000;
+                  top: 0;
+                  left: 0;
+                  background-color: red;`
+child.style = modalStyles;
+body.insertAdjacentElement('afterbegin', child);
+console.log(child);
 window.addEventListener('mouseup', getSelectionText);
 function getSelectionText() {
     var text = "";
